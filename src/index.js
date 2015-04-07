9d6e0bb4584da2d90c285ab5994675eb31796d86
Ian Henry <ianthehenry@gmail.com>
2015-04-06 23:02:30 -0400

    implement some behavior, though most tests are still failing

--- src/index.js
+++ src/index.js
@@ -1,3 +1,68 @@
 "use strict";
 
-module.exports = "brinks!";
+const makeNullaryConstructor = () => {
+  return function() {
+    if (arguments.length > 0) {
+      throw new Error("Cannot provide arguments to a nullary constructor");
+    }
+    return constructor;
+  };
+};
+
+const makePatternConstructor = () => {
+  const constructor = function(...fields) {
+    if (!(this instanceof constructor)) {
+      return new constructor(...fields);
+    }
+
+    this.get = function(i) {
+      if (arguments.length === 0) {
+        return fields;
+      } else {
+        return fields[i];
+      }
+    };
+  };
+
+  return constructor;
+};
+
+const makeConstructor = (type, pattern) => {
+  let constructor;
+  if (pattern.length === 0) {
+    constructor = makeNullaryConstructor();
+  } else {
+    constructor = makePatternConstructor(pattern);
+  }
+  constructor.prototype = Object.create(type.prototype, {
+    constructor: {
+      value: constructor,
+      enumerable: false,
+      writable: false,
+      configurable: false
+    }
+  });
+  return constructor;
+};
+
+module.exports = (setupFunction) => {
+  if (typeof setupFunction !== 'function') {
+    throw new Error("Must invoke brinks with a setup function");
+  }
+  // if (setupFunction.length > 0) {
+  //   throw new Error("Parameterized types are not implemented yet");
+  // }
+
+  const type = function() {};
+  const constructors = setupFunction.call(type);
+
+  for (let constructorName in constructors) {
+    if (!constructors.hasOwnProperty(constructorName)) {
+      continue;
+    }
+    const pattern = constructors[constructorName];
+    type[constructorName] = makeConstructor(type, pattern);
+  }
+
+  return type;
+};
